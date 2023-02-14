cc704a290fa31ce14e3020b4163b7de771b0ea15
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-14 20:59:57 +0530

    update file.

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ var thrid = Number(prompt("Enter the 3ed number"));
 
 if ((first > second) && (first > thrid)) {
     if (second > thrid) {
-        console.log("first is large , thrid is small ");
+        console.log("second is large , thrid is small");
     }else {
-        console.log("first is large , second is small ");
+        console.log("third is large , second is small ");
     }
 }else if ((second > first) && (second > thrid)) {
-    if (first> thrid) {
-        console.log("second is large ,  thrid is small");
+    if (first > thrid) {
+        console.log("first is large ,  thrid is small");
     }else {
-        console.log("second is large ,  first is small");
+        console.log("third is large ,  first is small");
     }
 }else {
     if (second > first) {
-        console.log("thrid is large , first is small");
+        console.log("second is large , first is small");
     }else{
-        console.log("thrid is large , second is small");
+        console.log("first is large , second is small");
     }
 }
 
