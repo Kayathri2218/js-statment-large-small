10b2d8e8275b7d69fdeb53418084bea7dee0c98e
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-15 01:48:25 +0530

    update javascript

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-var first = Number(prompt("Enter the 1st number"));
-var second = Number(prompt("Enter the 2nd number"));
-var thrid = Number(prompt("Enter the 3ed number"));
 
-if ((first > second) && (first > thrid)) {
-    if (second > thrid) {
-        console.log("second is large , thrid is small");
-    }else {
-        console.log("third is large , second is small ");
+
+let a = Number(prompt("Enter the 1st number"));
+let b = Number(prompt("Enter the 2nd number"));
+let c = Number(prompt("Enter the 3ed number"));
+
+let largest, smallest;
+
+if (a > b) {
+    if (a > c) {
+        largest = a;
+    } else {
+        largest = c;
     }
-}else if ((second > first) && (second > thrid)) {
-    if (first > thrid) {
-        console.log("first is large ,  thrid is small");
-    }else {
-        console.log("third is large ,  first is small");
-    }
-}else {
-    if (second > first) {
-        console.log("second is large , first is small");
-    }else{
-        console.log("first is large , second is small");
+} else {
+    if (b > c) {
+        largest = b;
+    } else {
+        largest = c;
     }
 }
+
+if (a < b) {
+    if (a < c) {
+        smallest = a;
+    } else {
+        smallest = c;
+    }
+} else {
+    if (b < c) {
+        smallest = b;
+    } else {
+        smallest = c;
+    }
+}
+
+console.log("Largest number is:", largest);
+console.log("Smallest number is:", smallest);
 
